51b51ce357d9e45e622bdd7d1963e8b9b4ce83a1
Jordan Nieuwhof <jordan.nieuwhof@versapay.com>
2018-01-22 12:43:23 -0500

    Added state capability for marked invoices for payment

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -4,19 +4,33 @@ import { Button, Checkbox, Row, Table } from 'react-bootstrap'
 class Invoices extends Component {
   state = {
     invoices: [
-      { number: '12345', amount_cents: '10000' },
-      { number: '12345', amount_cents: '10000' },
-      { number: '12345', amount_cents: '10000' },
-      { number: '12345', amount_cents: '10000' },
-      { number: '12345', amount_cents: '10000' },
+      { number: '1', amount_cents: '1000' },
+      { number: '2', amount_cents: '2000' },
+      { number: '3', amount_cents: '3000' },
+      { number: '4', amount_cents: '4000' },
+      { number: '5', amount_cents: '5000' },
     ],
+    selectedInvoices: [],
+  }
+
+  selectInvoice = (e, invoice) => {
+    this.setState({
+      selectedInvoices: e.target.checked ?
+        [...this.state.selectedInvoices, invoice] :
+        this.state.selectedInvoices.filter(x => x !== invoice)
+    })
+  }
+
+  markInvoicesForPayment = () => {
+    let { selectedInvoices } = this.state
+    debugger
   }
 
   render() {
     let { invoices } = this.state
     return (
       <div className="invoices-container">
-        <Row><Button>Mark for Payment</Button></Row>
+        <Row><Button onClick={this.markInvoicesForPayment}>Mark for Payment</Button></Row>
         <Row className='top-buffer'>
           <Table striped bordered condensed hover>
             <thead>
@@ -30,7 +44,7 @@ class Invoices extends Component {
               { invoices.map((x,i) => (
                 <tr key={i}>
                   <td>
-                    <Checkbox />
+                    <Checkbox onClick={(e) => { this.selectInvoice(e,x) }} />
                   </td>
                   <td>{ x.number }</td>
                   <td>{ x.amount_cents }</td>
