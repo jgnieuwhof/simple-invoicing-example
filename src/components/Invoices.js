1261ca8275c70e9aead4b46511640d815b8d64ef
Jordan Nieuwhof <jordan.nieuwhof@versapay.com>
2018-01-22 12:21:13 -0500

    Converted to class in prep for redux / state changes

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -1,40 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Checkbox, Row, Table } from 'react-bootstrap'
 
-const invoiceData = [
-  { number: '12345', amount_cents: '10000' },
-  { number: '12345', amount_cents: '10000' },
-  { number: '12345', amount_cents: '10000' },
-  { number: '12345', amount_cents: '10000' },
-  { number: '12345', amount_cents: '10000' },
-]
+class Invoices extends Component {
+  state = {
+    invoices: [
+      { number: '12345', amount_cents: '10000' },
+      { number: '12345', amount_cents: '10000' },
+      { number: '12345', amount_cents: '10000' },
+      { number: '12345', amount_cents: '10000' },
+      { number: '12345', amount_cents: '10000' },
+    ],
+  }
 
-const Invoices = () => (
-  <div className="invoices-container">
-    <Row><Button>Mark for Payment</Button></Row>
-    <Row className='top-buffer'>
-      <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Invoice Number</th>
-            <th>Invoice Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          { invoiceData.map((x,i) => (
-            <tr key={i}>
-              <td>
-                <Checkbox />
-              </td>
-              <td>{ x.number }</td>
-              <td>{ x.amount_cents }</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Row>
-  </div>
-)
+  render() {
+    let { invoices } = this.state
+    return (
+      <div className="invoices-container">
+        <Row><Button>Mark for Payment</Button></Row>
+        <Row className='top-buffer'>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Invoice Number</th>
+                <th>Invoice Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              { invoices.map((x,i) => (
+                <tr key={i}>
+                  <td>
+                    <Checkbox />
+                  </td>
+                  <td>{ x.number }</td>
+                  <td>{ x.amount_cents }</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Row>
+      </div>
+    )
+  }
+}
 
 export default Invoices
